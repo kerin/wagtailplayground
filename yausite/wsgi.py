040991cc7f1d81b891629cc493825e62e7d9e8b6
Kerin Cosford <cosford@gmail.com>
2014-03-12 17:05:17 +0000

    Heroku :(

--- yausite/wsgi.py
+++ yausite/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yausite.settings")
+sys.path.append(os.path.dirname(__file__))
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
